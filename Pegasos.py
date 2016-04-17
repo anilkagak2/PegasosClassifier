365833f18359f7cd39f7de023c44b411a4ce31f4
Anil Kag <anilkagak2@gmail.com>
2016-04-17 14:37:55 +0530

    Refactored PegasosClassifierWithKernels to inherit from PegasosClassifier

--- Pegasos.py
+++ Pegasos.py
@@ -35,53 +35,68 @@ class PegasosClassifier:
 		# Default parameters for the algorithm, to be used in case the user does not explicitly provide them
 		self.DefaultRegularizationConst = 0.03
 		self.DefaultIterations = 100000
+		self.lmbda = self.DefaultRegularizationConst
+		self.T = self.DefaultIterations
 
 		# are we dealing with binary classification problem?
 		self._isBinaryClassification = False
+
+	# Parse the parameters dictionary passed by the user
+	def _parseParameters(self, parameters):
+		self.T = int(parameters[ITERATIONS]) if ITERATIONS in parameters else self.DefaultIterations
+		self.lmbda = float(parameters[REGULARIZATION_CONST]) if REGULARIZATION_CONST in parameters else self.DefaultRegularizationConst
+		self.classLabels = self._uniqueClassLabels([point.y for point in data])
+		self._isBinaryClassification = True if len(self.classLabels)==2 else False
 
 	# Y is the list of output for the given data,
 	# This function returns the unique class labels
 	def _uniqueClassLabels(self, Y):
 		return list(Counter(Y))
 
+	# default classifier parameters
+	def _defaultClassifierParameters(self, dataLen, featureVectorLen):
+		return ClassifierParameters( np.zeros((featureVectorLen)) )
+
+	# update the classifier for classLabel with ith data point (xi,yi)
+	def _updateClassifierWithDataPoint(self, classLabel, i, xi, yi, etaT):
+		if ( yi * np.dot(self.classifiers[classLabel].theta, xi)) < 1.0:
+			self.classifiers[classLabel].theta = (1.0- etaT*lmbda)*self.classifiers[classLabel].theta + (etaT*yi)*xi
+		else:
+			self.classifiers[classLabel].theta = (1.0- etaT*lmbda)*self.classifiers[classLabel].theta
+
 	# train the classifier on the given data with given parameters
 	def fit(self, data, parameters):
 		if (data is None) or (len(data)==0):
 			print "Invalid training set"
 			return
-	
-		T = int(parameters[ITERATIONS]) if ITERATIONS in parameters else self.DefaultIterations
-		lmbda = float(parameters[REGULARIZATION_CONST]) if REGULARIZATION_CONST in parameters else self.DefaultRegularizationConst
-		
-		self.classLabels = self._uniqueClassLabels([point.y for point in data])
-		self._isBinaryClassification = True if len(self.classLabels)==2 else False
+
+		self._parseParameters(parameters)
 
 		dataLen = len(data)
 		featureVectorLen = len( data[0].x )
 		for classLabel in self.classLabels:
 			# initialize the classifier theta's to 0 vector
-			self.classifiers[classLabel] = ClassifierParameters( np.zeros((featureVectorLen)) )
+			self.classifiers[classLabel] = self._defaultClassifierParameters(dataLen, featureVectorLen)
 		
 			# train the learner with single example till T iterations
-			for t in xrange(1,T+1):
+			for t in xrange(1,self.T+1):
 				i = random.randint(0, dataLen-1)
-				etaT = 1.0 / (lmbda * t)
-
+				etaT = 1.0 / (self.lmbda * t)
 				xi = data[i].x
 				yi = 1 if data[i].y == classLabel else -1
-			
-				if ( yi * np.dot(self.classifiers[classLabel].theta, xi)) < 1.0:
-					self.classifiers[classLabel].theta = (1.0- etaT*lmbda)*self.classifiers[classLabel].theta + (etaT*yi)*xi
-				else:
-					self.classifiers[classLabel].theta = (1.0- etaT*lmbda)*self.classifiers[classLabel].theta
-				
+				self._updateClassifierWithDataPoint(classLabel, i, xi, yi, etaT)
+
 			# training complete
 			print "Training complete for label : ", classLabel
-			print "Theta : ", self.classifiers[classLabel].theta
+			#print "Theta : ", self.classifiers[classLabel].theta
 
 			# Break after training one classifier for binary classification
 			if self._isBinaryClassification: break
-		
+
+	# return (confidence for class, class Label)
+	def _classLabelConfidenceForInput(self, classLabel, x):
+		return (np.dot(self.classifiers[classLabel].theta, x), classLabel)
+
 	# predict the class label for given input
 	def predict(self, x):
 		if len(self.classifiers) == 0:
@@ -91,13 +106,16 @@ class PegasosClassifier:
 		predictions = []
 		if self._isBinaryClassification:
 			classOne, classTwo = self.classLabels[0], self.classLabels[1]
-			
+
 			# while training we break the loop after training for the classOne
-			confidence = np.dot(self.classifiers[classOne].theta, x)
+			confidence, _ = self._classLabelConfidenceForInput(classOne, x)
 			predictedClass = classOne if confidence>0 else classTwo
 			prediction = (confidence, predictedClass)
 		else:
-			predictions = [ (np.dot(self.classifiers[classLabel].theta, x), classLabel) for classLabel in self.classifiers ]
+			predictions = []
+			for classLabel in self.classifiers:
+				predictions.append( self._classLabelConfidenceForInput(classLabel, x) )
+
 			prediction = max(predictions)
 
 		#print "class predicted : ", prediction[1], " with confidence ", prediction[0]
@@ -114,109 +132,49 @@ def _gaussian(x1,x2):
 	return (math.e) ** ( differenceNorm / (2*(const_sigma**2)) )
 	
 # Pegasos algorithm with kernel tricks
-class PegasosClassifierWithKernels():
+class PegasosClassifierWithKernels(PegasosClassifier):
 	def __init__(self):
-		# we'll be using One-vs-All method for multi-class classification
-		# each class will be a key in this dictionary and the value will be the parameters for that class
-		self.classifiers = {}
-		self.classLabels = []
+		PegasosClassifier.__init__(self)
 
-		# Default parameters for the algorithm, to be used in case the user does not explicitly provide them
-		self.DefaultRegularizationConst = 0.03
-		self.DefaultIterations = 10000
+		# override the default iterations
+		self.DefaultIterations = 1000
 
-		# are we dealing with binary classification problem?
-		self._isBinaryClassification = False
+		# kernel function pointer, used to calculate similarity between data points
 		self.kernel = _linear
-		self.lmbda = self.DefaultRegularizationConst
-		self.T = self.DefaultIterations
-		self.X = []
 
-	# Y is the list of output for the given data,
-	# This function returns the unique class labels
-	def _uniqueClassLabels(self, Y):
-		return list(Counter(Y))
-
-	# train the classifier on the given data with given parameters
-	def fit(self, data, parameters):
-		if (data is None) or (len(data)==0):
-			print "Invalid training set"
-			return
-
-		T = int(parameters[ITERATIONS]) if ITERATIONS in parameters else self.DefaultIterations
-		lmbda = float(parameters[REGULARIZATION_CONST]) if REGULARIZATION_CONST in parameters else self.DefaultRegularizationConst
+	# Parse the parameters passed by user
+	def _parseParameters(self, parameters):
+		PegasosClassifier._parseParameters(self, parameters)
 		self.kernel = parameters[KERNEL_CONST] if KERNEL_CONST in parameters else _linear
-		self.classLabels = self._uniqueClassLabels([point.y for point in data])
-		self._isBinaryClassification = True if len(self.classLabels)==2 else False
-		self.X = [point.x for point in data]
 
-		dataLen = len(data)
-		for classLabel in self.classLabels:
-			# initialize the classifier theta's to 0 vector
-			self.classifiers[classLabel] = ClassifierParameters( dok_matrix((1,dataLen)), {}, dok_matrix((1,dataLen)) )
+	# default classifier parameters
+	def _defaultClassifierParameters(self, dataLen, featureVectorLen):
+		return ClassifierParameters( dok_matrix((1,dataLen)), {}, dok_matrix((1,dataLen)) )
 
-			# train the learner with single example till T iterations
-			for t in xrange(1,T+1):
-				i = random.randint(0, dataLen-1)
-				etaT = 1.0 / (lmbda * t)
+	# update the classifier for classLabel with ith data point (xi,yi)
+	def _updateClassifierWithDataPoint(self, classLabel, i, xi, yi, etaT):
+		comp = 0
+		for ((r,c),alpha) in self.classifiers[classLabel].theta.iteritems():
+			yj = self.classifiers[classLabel].Y[r,c]
+			xj = self.classifiers[classLabel].X[c]
+			comp += alpha * yj * self.kernel(xi, xj)
 
-				xi = data[i].x
-				yi = 1 if data[i].y == classLabel else -1
+		if (yi*etaT*comp) < 1.0:
+			if self.classifiers[classLabel].theta[0,i] == 0.0:
+				self.classifiers[classLabel].X[i] = xi
+				self.classifiers[classLabel].Y[0,i] = yi
 
-				comp = 0
-				for ((r,c),alpha) in self.classifiers[classLabel].theta.iteritems():
-					yj = self.classifiers[classLabel].Y[r,c]
-					xj = self.classifiers[classLabel].X[c]
-					comp += alpha * yj * self.kernel(xi, xj)
+			self.classifiers[classLabel].theta[0,i] += 1
 
-				if (yi*etaT*comp) < 1.0:
-					if self.classifiers[classLabel].theta[0,i] == 0.0:
-						self.classifiers[classLabel].X[i] = xi
-						self.classifiers[classLabel].Y[0,i] = yi
+	# return (confidence for class, class Label)
+	def _classLabelConfidenceForInput(self, classLabel, x):
+		confidence = 0.0
+		for ((r,c),alpha) in self.classifiers[classLabel].theta.iteritems():
+			yj = self.classifiers[classLabel].Y[r,c]
+			xj = self.classifiers[classLabel].X[c]
+			confidence += alpha * yj * self.kernel(x, xj)
 
-					self.classifiers[classLabel].theta[0,i] += 1
-
-			# training complete
-			print "Training complete for label : ", classLabel
-			#print "Theta : ", self.classifiers[classLabel].theta
-
-			# Break after training one classifier for binary classification
-			if self._isBinaryClassification: break
-
-	# predict the class label for given input
-	def predict(self, x):
-		if len(self.classifiers) == 0:
-			print "Not trained yet"
-			return
-
-		predictions = []
-		if self._isBinaryClassification:
-			classOne, classTwo = self.classLabels[0], self.classLabels[1]
-
-			# while training we break the loop after training for the classOne
-			confidence = 0.0
-			for ((r,c),alpha) in self.classifiers[classOne].theta.iteritems():
-					yj = self.classifiers[classOne].Y[r,c]
-					xj = self.classifiers[classOne].X[c]
-					confidence += alpha * yj * self.kernel(x, xj)
-
-			predictedClass = classOne if confidence>0 else classTwo
-			prediction = (confidence, predictedClass)
-		else:
-			predictions = []
-			for classLabel in self.classifiers:
-				confidence = 0.0
-				for ((r,c),alpha) in self.classifiers[classLabel].theta.iteritems():
-					yj = self.classifiers[classLabel].Y[r,c]
-					xj = self.classifiers[classLabel].X[c]
-					confidence += alpha * yj * self.kernel(x, xj)
-
-				predictions.append( (confidence, classLabel) )
-
-			prediction = max(predictions)
-
-		#print "class predicted : ", prediction[1], " with confidence ", prediction[0]
-		return prediction[1]
+		return (confidence, classLabel)
 
 
 # returns data read from the file, treating each line as a data point
@@ -293,8 +251,9 @@ classifier.predict(np.array([5.9,3.0,5.1,1.8]))
 #	- Test/Train separation
 #	- Score: Accuracy, F1 score, Precision/Recall
 #	- KFold cross_validation
+#	- Publish numbers for standard SVC from scikit-learn, PegasosClassifier, PegasosClassifierWithKernels for Wine & Iris
 # Optimizations
 #	- DONE Numpy arrays for better performance/memory consumption
 #	- DONE Sparse vectors
 #	- Sparse dot product
-#	- Refactor the code for PegasosClassifier and PegasosClassifierWithKernels using inheritance
+#	- DONE Refactor the code for PegasosClassifier and PegasosClassifierWithKernels using inheritance
